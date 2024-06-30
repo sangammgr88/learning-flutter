cf3dbc46d2e2de36baf4f79079e6fd670e545834
sangammgr88 <sangamtmagar49@gmail.com>
2024-06-30 13:46:29 +0545

    day one

--- bin/dayone.dart
+++ bin/dayone.dart
@@ -1,5 +1,42 @@
-import 'package:dayone/dayone.dart' as dayone;
+void main(){
+String a = "sangam hai";
+int b = 10;
+num c = 10.2;  
+bool d = false;  //boolen 
+String e = "10";
+String f = "10.2";
+print(a);
+print(b);
+print(c);
+print(d);
+print(e);
 
-void main(List<String> arguments) {
-  print('Hello world: ${dayone.calculate()}!');
-}
+List<String> list = ["a","e","i","o","u"];
+print(list);
+
+Map<String, int> maps = {
+  "lovely" : 46,
+    "lo" : 6,
+  "ly" : 4,
+  };
+  print(maps);
+
+
+  String h = 'Magar\'s';
+  print(a + " " + h);
+ // print("$a $h");
+  int ints = int.parse(e);
+  print(ints.runtimeType);
+  double intse = double.parse(f);
+  print(intse.runtimeType);
+
+   var p;
+   p = 49;
+   p = "sanfam";
+   print(p);
+
+   dynamic k = 10;
+   k = "sanfha";
+   print(k);
+ } 
+
