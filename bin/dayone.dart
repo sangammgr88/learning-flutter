7eea3b275d397206de78afe8b27eb58177d21313
sangammgr88 <sangamtmagar49@gmail.com>
2024-07-02 17:23:30 +0545

    daythree

--- bin/dayone.dart
+++ bin/dayone.dart
@@ -140,39 +140,126 @@
 //   for(var i = 1; i <= 10; i++){
 //   print(i*5);
 //  }
-  List<String> list = ["a","e","i","o","u"];
-print(list.length);
-list.forEach((action)=> print(action));
- for (int i=0; i< list.length; i++){
-  print(list[i]);
- }
- print("for in");
- for(var i in list){
-  print(i);
- }
- int i = 10;
- while(i > 0){   //condition 
-  print(i);   //statement 
-  --i; //in / dec operation
- }
- do{                 //do while condition0
-  print(i);
-  i++;
- }while(i <=10);
- //continue and brerak
-for (var i = 0; i <= 10; i++) {
-  if (i == 2) {
-    break;
-  }
-  print(i);
-}
+//   List<String> list = ["a","e","i","o","u"];
+// print(list.length);
+// list.forEach((action)=> print(action));
+//  for (int i=0; i< list.length; i++){
+//   print(list[i]);
+//  }
+//  print("for in");
+//  for(var i in list){
+//   print(i);
+//  }
+//  int i = 10;
+//  while(i > 0){   //condition 
+//   print(i);   //statement 
+//   --i; //in / dec operation
+//  }
+//  do{                 //do while condition0
+//   print(i);
+//   i++;
+//  }while(i <=10);
+//  //continue and brerak
+// for (var i = 0; i <= 10; i++) {
+//   if (i == 2) {
+//     break;
+//   }
+//   print(i);
+// }
 
-for (var i = 0; i <= 10; i++) {
-  if (i == 2) {
-    continue;
-  }
-  print(i);
-}
- }
+// for (var i = 0; i <= 10; i++) {
+//   if (i == 2) {
+//     continue;
+//   }
+//   print(i);
+// }
+//  }
   
+//  void main(){
+  //int error = 10~/0;
+ //print(error);
+ //   try{
+ //   int error = 10~/0;
+ //   print(error);
+ // }catch(e){
+ //   print(e);
+ // }on IntegerDivisionByZeroException{
+ //  print("error");
+// }
+// // print("Hello");
+// // void printName( String name){
+// //   print("k chu $name");
+// // }
+// // printName("sangam");
+// // printName("dai");
+// // printName("hjur");
+
+// // void Add(){
+// //   print("${5+6}");
+// // }
+// // Add();
+// // int sub(int a, int b){
+// //   return a-b;
+// // }
+// // print(sub(5, 3));
+
+// }
+// import 'dart:ffi';
+
+// void main(){
+  // void info (String name, String age,num number, {String country="Nepal"} ){
+  //   print("My name is $name and my age is $age and my mobile number $number and my country is $country ");
+  // }
+  // info("sangam", "20",96, country: "USA");
+  //   info("rabin", "20",96);
+
+  //   void info ({required String name,required String age,required num number, required country }){
+  //   print("My name is $name and my age is $age and my mobile number $number and my country is $country ");
+  // }
+  // info(name: "sangam", age: "19", number: 9999, country: "Nepal");
+  //  info( age: "19",name: "sangam",country: "Nepal", number: 9999 );
+  //   info(name: "sangam", age: "19", number: 9999, country: "Nepal");
+
+//  void info (String name, String age, [String? country] ){
+//     print("My name is $name and my age is $age and  my country is $country ");
+//   }
+//   info("sangam", "20", );
+//     info("rabin", "20", "USA");
+
+//     var a = (){
+//       return "hello";
+//     };
+   
+// //global scope
+//     int f= 10;
+//     print(f);
+//  print(a());
+//     var two =(int c, int b){
+//       return c + b+f;
+//     };
+//     print(two(40,50));
+
+//     void one() => print(9+0);
+
+// int three(int c, int d) => c+d;
+//     print(three(20,30));
+// }
+
+  
+// void main(){
+//   List<int> a = [10, 20, 30];
+//   List<String> b = ["10", "30", "99"];
+//   List c = ["sangam", 19];
+//   print(c.indexOf("sangam"));
+//   print(c.length);
+//   c.add("magar");
+//   print(c);
+//   c.addAll(["50", "60"]);
+//   print(c);
+//   print(c[0]);
+//   print(c[2]);
+//   c[0]="55";
+//   print(c);
+//   print(c.remove("55"));
+// }
 
